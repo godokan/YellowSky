99dd5c49da08b08197203a0432a1fc172781a8c4
godokan <nonamekoo@naver.com>
2023-11-23 11:32:19 +0900

    로그인 레이아웃 구성

--- app/src/main/java/com/godokan/yellowsky/MainActivity.java
+++ app/src/main/java/com/godokan/yellowsky/MainActivity.java
@@ -3,7 +3,9 @@ package com.godokan.yellowsky;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,60 +18,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
-
-    private MarkerInfoTask infoTask = new MarkerInfoTask();
-
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        assert mapFragment != null;
-        mapFragment.getMapAsync((OnMapReadyCallback) this);
-    }
+        Button btn_login = findViewById(R.id.btn_login);
 
-    // NULL이 아닌 GoogleMap 객체를 파라미터로 제공해 줄 수 있을 때 호출
-    @Override
-    public void onMapReady(@NonNull final GoogleMap googleMap) {
-        mMap = googleMap;
-        List<ApiListMapDto> list;
-        InitNetMapList mapList = new InitNetMapList();
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.2410864, 127.1775537), 11));
-
-        mapList.start();
-        try {
-            mapList.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        list = mapList.getMarkers();
-
-        System.out.println(list.get(0).getProperName()+" "+list.get(0).getNo());
-
-        for (ApiListMapDto map : list) {
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(map.getLat(),map.getLng()))
-                    .title(map.getProperName())
-                    .snippet(map.getAddress())
-            );
-        }
-
-    }
-
-    class InitNetMapList extends Thread {
-        private List<ApiListMapDto> markers = new ArrayList<>();
-        @Override
-        public void run() {
-            markers = infoTask.getMarkerInfo();
-        }
-
-        public List<ApiListMapDto> getMarkers() {
-            return markers;
-        }
+        btn_login.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/godokan/yellowsky/MapActivity.java
@@ -0,0 +1,73 @@
+package com.godokan.yellowsky;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    private MarkerInfoTask infoTask = new MarkerInfoTask();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+    }
+
+    // NULL이 아닌 GoogleMap 객체를 파라미터로 제공해 줄 수 있을 때 호출
+    @Override
+    public void onMapReady(@NonNull final GoogleMap googleMap) {
+        mMap = googleMap;
+        List<ApiListMapDto> list;
+        InitNetMapList mapList = new InitNetMapList();
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.2410864, 127.1775537), 11));
+
+        mapList.start();
+        try {
+            mapList.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        list = mapList.getMarkers();
+
+        if(list!=null) {
+            for (ApiListMapDto map : list) {
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(map.getLat(),map.getLng()))
+                        .title(map.getProperName())
+                        .snippet(map.getAddress())
+                );
+            }
+        }
+
+    }
+
+    private class InitNetMapList extends Thread {
+        private List<ApiListMapDto> markers = new ArrayList<>();
+        @Override
+        public void run() {
+            markers = infoTask.getMarkerInfo();
+        }
+
+        public List<ApiListMapDto> getMarkers() {
+            return markers;
+        }
+    }
+}
