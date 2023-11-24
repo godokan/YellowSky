3c587e853ec9077cad13628046620a832e006769
godokan <117326245+godokan@users.noreply.github.com>
2023-11-24 16:55:43 +0900

    로그인 요청 기능 작성

--- app/src/main/java/com/godokan/yellowsky/MainActivity.java
+++ app/src/main/java/com/godokan/yellowsky/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
+    private static final MemberTask memberTask = new MemberTask();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,54 @@ public class MainActivity extends AppCompatActivity{
         TextView tv_signup = findViewById(R.id.tv_signup);
 
         btn_login.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), MapActivity.class);
-            startActivity(intent);
+            String id = ed_id.getText().toString();
+            String pw = ed_pw.getText().toString();
+
+            if (id.length()==0)
+                Toast.makeText(this.getApplicationContext(), "아이디를 입력하시기 바랍니다", Toast.LENGTH_LONG).show();
+            else if (pw.length()==0)
+                Toast.makeText(this.getApplicationContext(), "비밀번호를 입력하시기 바랍니다", Toast.LENGTH_LONG).show();
+            else {
+                PostNetLogin netLogin = new PostNetLogin(id, pw);
+
+                netLogin.start();
+
+                try {
+                    netLogin.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                boolean result = netLogin.getLoginResult();
+                if (result) {
+                    Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this.getApplicationContext(), "로그인 실패", Toast.LENGTH_LONG).show();
+                }
+            }
         });
+    }
+
+    private class PostNetLogin extends Thread{
+        private String id;
+        private String pw;
+        private boolean result = false;
+
+        public PostNetLogin(String id, String pw) {
+            this.id = id;
+            this.pw = pw;
+        }
+
+        @Override
+        public void run() {
+            result = memberTask.loginTask(id, pw);
+        }
+
+        public boolean getLoginResult() {
+            return result;
+        }
+
+
     }
 }
--- app/src/main/java/com/godokan/yellowsky/MemberTask.java
+++ app/src/main/java/com/godokan/yellowsky/MemberTask.java
@@ -1,25 +1,66 @@
 package com.godokan.yellowsky;
 
 
-import java.net.MalformedURLException;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class MemberTask {
     private final String  URL = "http://ccsyasu.cafe24.com:8082/";
 
+    // 로그린
     public boolean loginTask(String id, String pw) {
         String result = null;
+        MemberDTO memberDTO;
         try {
-            URL url = new URL(URL+"?id="+id+"&pw="+pw);
-            //TODO : YellowSkyAPI와 통신 로직 작성
-            // POST방식, 결과코드 "ERR_USER_NOT_FOUND", "ERR_PW_NOT_MATCH", "OK"
-            // 각각의 결과에 대응할것
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            URL url = new URL(URL+"api/login?id="+id+"&pw="+pw);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type","application/json;charset=UTF-8");
+            InputStream is = conn.getInputStream();
+            StringBuilder builder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            // Set the result
+            result = builder.toString();
+            System.out.println(result);
+
+        } catch (Exception e) {
+            Log.e("YELLOW_SKY_REST_API", "POST method failed: " + e.getMessage());
+            e.printStackTrace();
         }
 
+        if (result!=null) {
+            switch (result) {
+                case "OK" -> {
+                    return true;
+                }
+                case "ERR_USER_NOT_FOUND", "ERR_PW_NOT_MATCH" -> {
+                    return false;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+        return false;
     }
 
     //TODO : 회원 가입 요청 작성
+
+    //TODO : ID 중복 체크 로직
+    //TODO : 닉네임 중복 체크 로직
+    //TODO : 세션 유효 체크 로직
+
+
 
 }
